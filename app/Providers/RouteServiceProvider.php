a6c492c7acc93fb55e1f1917f1f1686ce87a2226
Niraj Dhakal <49120977+pxlcodes@users.noreply.github.com>
2021-09-02 20:16:24 +0545

    fix(Routes): route cleanup

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,16 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/auth.php'));
+
+            Route::middleware(['web','isAdmin'])
+                ->name('admin.')
+                ->prefix('admin')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
