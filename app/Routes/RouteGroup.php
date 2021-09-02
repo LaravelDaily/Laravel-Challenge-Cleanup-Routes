ca418b5a7ab33968d7cf4910a4adc2cc47b79b79
uutkukorkmaz <uutkukorkmaz>
2021-09-02 12:04:00 +0300

    Route organizing structure established

---
+++ app/Routes/RouteGroup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Routes;
+
+/**
+ * Interface of route organizing classes
+ *
+ * @author Utku Korkmaz <uutkukorkmaz@gmail.com>
+*/
+interface RouteGroup
+{
+    public static function register();
+}
