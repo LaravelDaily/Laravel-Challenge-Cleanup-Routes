ee18925e7f29ae3ae898dd0b00a04262e6965d75
MHYeganeh <mhyeganeh73@gmail.com>
2021-09-03 01:41:22 +0430

    Routes Cleanup & Refactoring

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,12 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+	        Route::middleware(['web', 'isAdmin'])
+		        ->namespace($this->namespace)
+		        ->prefix('admin/')
+		        ->as('admin.')
+		        ->group(base_path('routes/admin.php'));
         });
     }
 
