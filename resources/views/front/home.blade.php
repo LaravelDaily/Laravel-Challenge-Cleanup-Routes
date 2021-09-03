8eaf9fe01470f575a9dd435c0cc8d0fc90cd87c3
Subhabrata Kundu <skundu.creative@gmail.com>
2021-09-03 22:33:58 +0530

    Routes cleanup challenge completed

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -2,7 +2,7 @@
     <section class="text-gray-600 body-font">
         <div class="container max-w-7xl px-5 mx-auto mt-6">
             <div class="p-6 bg-white border-b border-gray-200">
-                {{ __('Home Page') }}
+                {{ __('Landing Page') }}
             </div>
         </div>
     </section>
