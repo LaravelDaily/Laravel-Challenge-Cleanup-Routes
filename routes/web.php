9c9013dc117c03dc2c86829a0fba3bd5906a046b
2nd user <2nd-email@email.cc>
2021-09-03 16:21:11 +0200

    Challenge With a Different way

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use \App\Http\Controllers\{HomeController,BookController,BookReportController,OrderController,UserSettingsController,UserChangePassword};
+use \App\Http\Controllers\Admin\{AdminDashboardController,AdminBookController,AdminUsersController};
+
+/**
+ * -- My Notes --
+ * 1- I installed  laravel/ui package to use Auth::routes();  instead of require __DIR__ . '/auth.php';
+ * 2- Problem : when i used 'namespace'=>'Admin' or 'namespace'=>'\App\Http\Controllers\Admin' inside route group array
+ *   It works only with routes witch calling class without function parm. like AdminDashboardController::class
+ *   But when calling class ass array with function parm it don't work :( like [AdminBookController::class, 'index']
+ *   So i resolved this by use all classes like above on this file .
+ */
 
 /*
 |--------------------------------------------------------------------------
@@ -13,37 +25,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', \App\Http\Controllers\HomeController::class)->name('home');
-Route::get('book/create', [\App\Http\Controllers\BookController::class, 'create'])->middleware('auth')->name('books.create');
-Route::post('book/store', [\App\Http\Controllers\BookController::class, 'store'])->middleware('auth')->name('books.store');
-Route::get('book/{book:slug}/report/create', [\App\Http\Controllers\BookReportController::class, 'create'])->middleware('auth')->name('books.report.create');
-Route::post('book/{book}/report', [\App\Http\Controllers\BookReportController::class, 'store'])->middleware('auth')->name('books.report.store');
-Route::get('book/{book:slug}', [\App\Http\Controllers\BookController::class, 'show'])->name('books.show');
+Route::get('/', HomeController::class)->name('home');
 
-Route::get('user/books', [\App\Http\Controllers\BookController::class, 'index'])->middleware('auth')->name('user.books.list');
-Route::get('user/books/{book:slug}/edit', [\App\Http\Controllers\BookController::class, 'edit'])->middleware('auth')->name('user.books.edit');
-Route::put('user/books/{book:slug}', [\App\Http\Controllers\BookController::class, 'update'])->middleware('auth')->name('user.books.update');
-Route::delete('user/books/{book}', [\App\Http\Controllers\BookController::class, 'destroy'])->middleware('auth')->name('user.books.destroy');
+Route::group(['prefix'=>'book','as'=>'books.'],function(){
 
-Route::get('user/orders', [\App\Http\Controllers\OrderController::class, 'index'])->middleware('auth')->name('user.orders.index');
+    Route::group(['middleware'=>'auth'],function(){
+        Route::get('create', [BookController::class, 'create'])->name('create');
+        Route::post('store', [BookController::class, 'store'])->name('store');
+        Route::get('{book:slug}/report/create', [BookReportController::class, 'create'])->name('report.create');
+        Route::post('{book}/report', [BookReportController::class, 'store'])->name('report.store');
+    });
+    Route::get('{book:slug}', [BookController::class, 'show'])->name('show');
 
-Route::get('user/settings', [\App\Http\Controllers\UserSettingsController::class, 'index'])->middleware('auth')->name('user.settings');
-Route::post('user/settings/{user}', [\App\Http\Controllers\UserSettingsController::class, 'update'])->middleware('auth')->name('user.settings.update');
-Route::post('user/settings/password/change/{user}', [\App\Http\Controllers\UserChangePassword::class, 'update'])->middleware('auth')->name('user.password.update');
+});
 
-Route::get('admin', \App\Http\Controllers\Admin\AdminDashboardController::class)->middleware('isAdmin')->name('admin.index');
 
-Route::get('admin/books', [\App\Http\Controllers\Admin\AdminBookController::class, 'index'])->middleware('isAdmin')->name('admin.books.index');
-Route::get('admin/books/create', [\App\Http\Controllers\Admin\AdminBookController::class, 'create'])->middleware('isAdmin')->name('admin.books.create');
-Route::post('admin/books', [\App\Http\Controllers\Admin\AdminBookController::class, 'store'])->middleware('isAdmin')->name('admin.books.store');
-Route::get('admin/books/{book}/edit', [\App\Http\Controllers\Admin\AdminBookController::class, 'edit'])->middleware('isAdmin')->name('admin.books.edit');
-Route::put('admin/books/{book}', [\App\Http\Controllers\Admin\AdminBookController::class, 'update'])->middleware('isAdmin')->name('admin.books.update');
-Route::delete('admin/books/{book}', [\App\Http\Controllers\Admin\AdminBookController::class, 'destroy'])->middleware('isAdmin')->name('admin.books.destroy');
-Route::put('admin/book/approve/{book}', [\App\Http\Controllers\Admin\AdminBookController::class, 'approveBook'])->middleware('isAdmin')->name('admin.books.approve');
+Route::group(['prefix'=>'user','as'=>'user.','middleware'=>'auth'],function(){
 
-Route::get('admin/users', [\App\Http\Controllers\Admin\AdminUsersController::class, 'index'])->middleware('isAdmin')->name('admin.users.index');
-Route::get('admin/users/{user}/edit', [\App\Http\Controllers\Admin\AdminUsersController::class, 'edit'])->middleware('isAdmin')->name('admin.users.edit');
-Route::put('admin/users/{user}', [\App\Http\Controllers\Admin\AdminUsersController::class, 'update'])->middleware('isAdmin')->name('admin.users.update');
-Route::delete('admin/users/{user}', [\App\Http\Controllers\Admin\AdminUsersController::class, 'destroy'])->middleware('isAdmin')->name('admin.users.destroy');
+    Route::get('books', [BookController::class, 'index'])->name('books.list');
+    Route::get('books/{book:slug}/edit', [BookController::class, 'edit'])->name('books.edit');
+    Route::put('books/{book:slug}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
 
-require __DIR__ . '/auth.php';
+    Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
+
+    Route::get('settings', [UserSettingsController::class, 'index'])->name('settings');
+    Route::post('settings/{user}', [UserSettingsController::class, 'update'])->name('settings.update');
+    Route::post('settings/password/change/{user}', [UserChangePassword::class, 'update'])->name('password.update');
+
+});
+
+
+//'namespace'=>'Admin' working only with >> oute::get('/', AdminDashboardController::class)->name('index');
+Route::group(['prefix'=>'admin','as'=>'admin.','middleware'=>'isAdmin'],function(){
+
+    Route::get('/', AdminDashboardController::class)->name('index');
+
+    Route::get('books', [AdminBookController::class, 'index'])->name('books.index');
+    Route::get('books/create', [AdminBookController::class, 'create'])->name('books.create');
+    Route::post('books', [AdminBookController::class, 'store'])->name('books.store');
+    Route::get('books/{book}/edit', [AdminBookController::class, 'edit'])->name('books.edit');
+    Route::put('books/{book}', [AdminBookController::class, 'update'])->name('books.update');
+    Route::delete('books/{book}', [AdminBookController::class, 'destroy'])->name('books.destroy');
+    Route::put('book/approve/{book}', [AdminBookController::class, 'approveBook'])->name('books.approve');
+
+    Route::get('users', [AdminUsersController::class, 'index'])->name('users.index');
+    Route::get('users/{user}/edit', [AdminUsersController::class, 'edit'])->name('users.edit');
+    Route::put('users/{user}', [AdminUsersController::class, 'update'])->name('users.update');
+    Route::delete('users/{user}', [AdminUsersController::class, 'destroy'])->name('users.destroy');
+});
+
+// with laravel/ui package
+Auth::routes(); 
+
+#require __DIR__ . '/auth.php';
