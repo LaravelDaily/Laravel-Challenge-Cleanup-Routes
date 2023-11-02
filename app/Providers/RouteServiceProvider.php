9389ea1fbbfa076c31bfe4bae068cc54ed629157
Mohammad Salah <thesalahrand@gmail.com>
2023-11-02 12:43:21 +0600

    chore: challenge completed

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,12 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware(['web', 'isAdmin'])
+                ->prefix('admin/')
+                ->name('admin.')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
---
+++ routes/admin.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\{
+  AdminDashboardController,
+  AdminBookController,
+  AdminUsersController
+};
+
+Route::get('/', AdminDashboardController::class)->name('index');
+Route::resource('books', AdminBookController::class)->except('show');
+Route::put('book/approve/{book}', [AdminBookController::class, 'approveBook'])->name('books.approve');
+Route::resource('users', AdminUsersController::class)->except(['create', 'store', 'show']);
